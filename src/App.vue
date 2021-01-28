2b8190559afee5ee72efc2e6205d1936f94558bc
JYC0413 <840732889@qq,com>
2021-01-28 11:11:44 +0800

    考试管理基础框架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-    <el-checkbox-group v-model="checkList">
-      <el-checkbox label="复选框 A"></el-checkbox>
-      <el-checkbox label="复选框 B"></el-checkbox>
-      <el-checkbox label="复选框 C"></el-checkbox>
-      <el-checkbox label="禁用" disabled></el-checkbox>
-      <el-checkbox label="选中且禁用" disabled></el-checkbox>
-    </el-checkbox-group>
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,11 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  data () {
-    return {
-      checkList: ['选中且禁用','复选框 A']
-    };
-  },
   components: {
     HelloWorld
   }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui';
+import echarts from 'echarts'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
+
+Vue.prototype.$echarts = echarts
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
