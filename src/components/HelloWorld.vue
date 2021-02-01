9792d77f3091282f7932e2747ce67fbc1628b57f
JYC0413 <840732889@qq,com>
2021-02-01 16:29:31 +0800

    已完成页面样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,205 +1,180 @@
 <template>
-  <div class="hello">
-    <div align="center">
-      <div style="width: 60%;height: 500px;border:1px solid rgb(180,180,180)" id="homeChart"></div>
-      <el-card style="width: 80%" class="box-card">
-        <div >
-        <el-table
-            :data="tableData"
-            :header-cell-style="{background:'#F0F4FF'}"
-            :row-style="{height: '0',padding: '0px'}"
-            :cell-style="{padding: '0px'}"
-            v-loading="listLoading"
-            element-loading-text="Loading"
-            fit
-            highlight-current-row
-            @row-click="clickTable"
-            ref="refTable"
-            :class="{activeColor:colorIndex===index}"
-        >
-          <el-table-column type="expand">
-            <template slot-scope="scope">
-              <el-tabs tab-position="left">
-                <el-tab-pane label="实验统计"><span @click="_goEntDetail( scope.row.reason )"></span>{{ scope.row.reason }}</el-tab-pane>
-                <el-tab-pane label="问题记录">{{ scope.row.advice }}</el-tab-pane>
-                <el-tab-pane label="建议">{{ scope.row.reference }}</el-tab-pane>
-                <el-tab-pane label="提交历史">{{ scope.row.history }}</el-tab-pane>
-              </el-tabs>
-            </template>
-          </el-table-column>
-          <el-table-column
-              type="selection"
-              width="55">
-          </el-table-column>
-          <el-table-column
-              align="center"
-              label="#"
-              width="60">
-            <template slot-scope="scope">
-              {{ scope.$index + 1 }}
-            </template>
-          </el-table-column
-          >
-          <el-table-column label="姓名">
-            <template slot-scope="scope">
-              <span>{{ scope.row.orderNumber }}</span>
-            </template>
-          </el-table-column>
-
-          <el-table-column align="center" label="院校">
-            <template slot-scope="scope">
-              <span>{{ scope.row.company }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="专业">
-            <template slot-scope="scope">
-              {{ scope.row.clientName }}
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="班级">
-            <template slot-scope="scope">
-              {{ scope.row.clientPhone }}
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="学号">
-            <template slot-scope="scope">
-              {{ scope.row.clientAddress }}
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="实验开始时间">
-            <template slot-scope="scope">
-              {{ scope.row.person }}
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="实验结束时间">
-            <template slot-scope="scope">
-              {{ scope.row.orderCreateDate }}
-            </template>
-          </el-table-column>
-
-          <el-table-column align="center" label="实验用时" width="160">
-            <template slot-scope="scope">
-              {{ scope.row.time }}
-            </template>
-          </el-table-column>
-          <el-table-column align="center" label="实验成绩" width="160">
-            <template slot-scope="scope">
-              {{ scope.row.grade }}
-            </template>
-          </el-table-column>
-        </el-table>
+  <div>
+    <el-row style="padding:0 40px" type="flex" justify="space-between">
+      <el-col :span="8">
+        <el-tabs v-model="activeName" tabPosition="bottom" @tab-click="color=!color" stretch="ture">
+          <el-tab-pane label="项目介绍视频" name="first" :class="{a: color}" @tab-click="changeColor(this)">
+            <div class='demo'>
+              <video-player class="video-player vjs-custom-skin"
+                            ref="videoPlayer"
+                            :playsinline="true"
+                            :options="playerOptions">
+              </video-player>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="教学引导视频" name="second" @tab-click="changeColor(this)">
+            <div class='demo'>
+              <video-player class="video-player vjs-custom-skin"
+                            ref="videoPlayer"
+                            :playsinline="true"
+                            :options="playerOptions">
+              </video-player>
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col :span="15" style="text-align:left; ">
+        <h2 style="margin: 0px">虚拟实验标题虚拟实验标题虚拟实验标题</h2>
+        <el-rate
+            v-model="value"
+            disabled
+            show-score
+            :colors="colors"
+            style="padding: 10px 0"
+            text-color="#ff9900"
+            score-template="{value}">
+        </el-rate>
+        <h4 style="margin: 0px">实验学时：4</h4>
+        <h4 style="margin: 0px">实验负责人：XX</h4>
+        <p style="font-size: 14px">实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介实验简介</p>
+        <el-button style="color: #fff;background-color: #FF9E00;font-size: 25px;padding: 20px" size="medium">查看实验报告</el-button>
+      </el-col>
+    </el-row>
+    <el-divider></el-divider>
+      <el-row style="padding:0 40px" type="flex" justify="space-around">
+        <el-col :span="7">
+          <el-card class="box-card">
+            <div class="card">
+              <i style="font-size: 35px" class="el-icon-edit"></i>
+              课前预习
+            </div>
+            <div style="text-align: left;height: 100px">
+              <p>查看本实验学习资料</p>
+              <p>掌握学习内容</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="7">
+          <el-card class="box-card">
+            <div class="card">
+            <i style="font-size: 35px" class="el-icon-document"></i>
+            考题测验
+            </div>
+            <div style="text-align: left;height: 100px">
+              <p>根据学习内容进行试题考核</p>
+              <p>检验对知识内容的掌握情况</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="7">
+          <el-card class="box-card">
+            <div class="card">
+            <i style="font-size: 35px" class="el-icon-attract"></i>
+            开始实验
+            </div>
+            <div style="text-align: left;height: 100px">
+              <p>进入虚拟仿真实验里</p>
+              <p>真实环境更好掌握实验内容</p>
+              <el-button style="border-radius: 10px;background-color: #FF9E00;color: white;font-weight: bolder;font-size: 13px;width: 30%;float: right">客户端下载</el-button>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    <el-divider></el-divider>
+    <div style="width: 80%;margin: 0 auto;text-align: left">
+      <div style="padding:5px 15px;background-color: #FF9E00;color: white;font-weight: bolder;font-size: 15px;">
+        实验讨论
+      </div>
+      <div style="width: 100%;box-sizing: border-box;border: 1px solid #BBBBBB;padding: 10px 40px">
+        <div style="display: flex;">
+          <span style="font-size: 16px;font-weight: bolder;padding-top: 1px">综合评分</span>
+          <el-rate
+              v-model="value1"
+              disabled
+              show-score
+              :colors="colors"
+              text-color="#ff9900"
+              score-template="{value}">
+          </el-rate>
+          <span style="font-size: 12px;padding-top: 4px">(2人评价)</span>
         </div>
-      </el-card>
+        <el-divider></el-divider>
+        <div style="display: flex;">
+        <span>我的评价：</span>
+        <el-rate
+            v-model="value2"
+            disabled
+            show-score
+            :colors="colors"
+            text-color="#ff9900"
+            score-template="{value}">
+        </el-rate>
+        </div>
+        <span>超级赞的实验课程</span>
+      </div>
     </div>
   </div>
 </template>
-
 <script>
-let echarts = require('echarts');
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
   data() {
     return {
-      // 获取row的key值
+      value: 4.5,
+      value1: 5.0,
+      value2: 5.0,
+      colors: ['#99A9BF', '#F7BA2A', '#FF9900'],
+      color:true,
       activeName: 'first',
-      listLoading: false,
-      isShow:false,
-      listQuery: {
-        value: 1
-      },
-      tableData: [{
-        orderNumber: '张三',
-        company: '大数据学院',
-        clientName: '软件工程',
-        clientPhone: '1班',
-        clientAddress: '20181205071',
-        person: '2021.1.1',
-        orderCreateDate: '2021.1.1',
-        time: '120分钟',
-        grade: '99',
-        reason: '展开详情内容',
-        advice: '展开详情内容',
-        reference: '展开详情内容',
-        history: '展开详情内容',
-      },
-        {
-          orderNumber: '张三',
-          company: '大数据学院',
-          clientName: '软件工程',
-          clientPhone: '1班',
-          clientAddress: '20181205071',
-          person: '2021.1.1',
-          orderCreateDate: '2021.1.1',
-          time: '120分钟',
-          grade: '99',
-          reason: '展开详情内容',
-          advice: '展开详情内容',
-          reference: '展开详情内容',
-          history: '展开详情内容',
-        },
-        {
-          orderNumber: '张三',
-          company: '大数据学院',
-          clientName: '软件工程',
-          clientPhone: '1班',
-          clientAddress: '20181205071',
-          person: '2021.1.1',
-          orderCreateDate: '2021.1.1',
-          time: '120分钟',
-          grade: '99',
-          reason: '展开详情内容',
-          advice: '展开详情内容',
-          reference: '展开详情内容',
-          history: '展开详情内容',
-        }]
-    };
-  },
-  methods: {
-    // eslint-disable-next-line
-    clickTable(row, index, e) {
-      this.$refs.refTable.toggleRowExpansion(row)
-      this.isShow=!this.isShow
-      this.colorIndex = index;
+      playerOptions: {
+        playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
+        autoplay: false, // 如果为true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 是否视频一结束就重新开始。
+        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [{
+          type: "video/mp4", // 类型
+          src: 'https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4' // url地址
+        }],
+        poster: '', // 封面地址
+        notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true, // 当前时间和持续时间的分隔符
+          durationDisplay: true, // 显示持续时间
+          remainingTimeDisplay: false, // 是否显示剩余时间功能
+          fullscreenToggle: true // 是否显示全屏按钮
+        }
+      }
     }
   },
-  mounted(){
-    let myChart = echarts.init(document.getElementById('homeChart'));
-    myChart.setOption(
-        {
-          title: {text: '人次'},
-          tooltip: {},
-          xAxis: {
-            data: ["01-01","01-02","01-03","01-04","01-05","01-06"]
-          },
-          yAxis:{},
-          series:[{
-            name:'销量',
-            type:'line',
-            data: [5,20,36,10,10,20]
-          }]
-        }
-    );
+  methods: {
+
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.el-icon-star-on{
+  font-size: 25px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.card{
+  border-radius: 10px;
+  padding: 15px 0;
+  background-color: #FF9E00;
+  color: white;
+  font-weight: bolder;
+  font-size: 25px;
+  width: 85%;
 }
 </style>
+
+"pages":[
+"pages/XXX/XXX"
+]
+
+XXX:function (options){
+wx.navigateTo({
+url:''
+})
+}
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import ElementUI from 'element-ui';
 import echarts from 'echarts'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
-
+import VideoPlayer from 'vue-video-player';
+import 'vue-video-player/src/custom-theme.css'
+import 'video.js/dist/video-js.css'
+import 'videojs-flash'
+import 'videojs-contrib-hls'
 
 Vue.prototype.$echarts = echarts
 Vue.use(ElementUI);
+Vue.use(VideoPlayer);
 Vue.config.productionTip = false
 
 new Vue({
