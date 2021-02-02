1db84c6fdc1b31e22fe2efc3d0190599db9cca72
wangyan <1130084903@qq.com>
2021-02-02 11:47:33 +0800

    http请求路径修改

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 export function login(data){
     return request({
-        url:'/admin/login',
+        url:'/api/login',
         method:'post',
         data:data//?
     })
