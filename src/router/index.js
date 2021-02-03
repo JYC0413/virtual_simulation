e56917f82bce77f61a0e1dffc8c37eb3a072d287
xu1wei <wrain_wind@163.com>
2021-02-03 09:59:53 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 Vue.use(VueRouter)
 
 import ExperimentEditor from '@/views/Experiment editor/index.vue'
-
+import Toutal from '@/header/index.vue'
 export default new VueRouter({
   mode:'history',
 
@@ -12,6 +12,20 @@ export default new VueRouter({
         {
             path:'/',
             component:()=>import('@/views/login/index.vue')
+        },
+        {
+            path:'total',
+            component:Toutal,
+            children:[
+                {
+                    path: '/shouye',
+                    component: () => import('@/views/shouye/index')
+                },
+                {
+                    path: '/teacher',
+                    component: () => import('@/views/teacher/index')
+                },
+            ]
         },
     {
         path:'/ExperimentEditor',
@@ -21,14 +35,7 @@ export default new VueRouter({
                 path:'/synopsis',
                 component:()=>import('@/views/Experiment editor/synopsis.vue')
             },
-           {
-                    path: '/shouye',
-                    component: () => import('@/views/shouye/index')
-                },
-                {
-                    path: '/teacher',
-                    component: () => import('@/views/teacher/index')
-                },
+           
         ]
     },
     {
